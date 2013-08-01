f0320f6d85dab3520607af68c6e5c8d260ccd4ad
Leo Lou <louyuhong@gmail.com>
2013-08-01 10:23:12 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AddthisTurbolinksExample::Application.config.secret_key_base = 'a8380d0278846772ad849ec35953a66a0f5cc8ae1454b25a6ea5c667ac1e2e9713161c1e390f6b97a677aa020193a8f9d0721864d9f17fed0e86949590c10eef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AddthisTurbolinksExample::Application.config.session_store :cookie_store, key: '_addthis-turbolinks-example_session'
