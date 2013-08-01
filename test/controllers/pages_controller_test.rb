739e73fb6c85bd3f854284981c873eae34a24d62
Leo Lou <louyuhong@gmail.com>
2013-08-01 10:23:30 +0800

    controller

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get one" do
+    get :one
+    assert_response :success
+  end
+
+  test "should get two" do
+    get :two
+    assert_response :success
+  end
+
+end
